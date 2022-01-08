3d42252befa36a439462945d82e58a88c3bb2a75
ManikDev <manikant@ultrahuman.com>
2022-01-08 20:54:27 +0530

    final touchUp done

---
+++ app/src/main/java/com/example/todoapp/splashscreen.kt
@@ -0,0 +1,19 @@
+package com.example.todoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
